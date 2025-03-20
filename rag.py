e3477bbc7e5cf918b86c419eac8285d929607b9c
Elisei564 <batmansgrob@yandex.ru>
2025-03-20 16:43:33 +0300

    Update RAG

--- rag.py
+++ rag.py
@@ -1,21 +1,46 @@
-from langchain_community.vectorstores import FAISS
-import os
 from langchain_huggingface import HuggingFaceEmbeddings
+import streamlit as st
+from pinecone import Pinecone
+import os
+
+model_name = "sergeyzh/BERTA"
+model = HuggingFaceEmbeddings(model_name = model_name)
 
 
-model = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-small")
+try:
+    pc = Pinecone(
+        api_key= st.secrets['PINECONE_API_KEY']
+    )
+except Exception as e:
+    pc = None
 
-db = os.path.join(os.curdir, "db") 
+index_name = "berta-index"
+try:
+    index = pc.Index(index_name) if pc else None
+except Exception as e:
+    index = None
 
-if not os.path.exists(db):
-    raise FileNotFoundError(f"The directory {db} does not exist. Please check the path.")
-
-vector_store = FAISS.load_local(db, model, allow_dangerous_deserialization=True)
 
 
-def context(query):
+def context(query_text):
 
-    retriever = vector_store.similarity_search(query, k=5)
+    if not pc:
+        raise RuntimeError(f"Pinecone doesn't exist")
+    if not index:
+        raise RuntimeError(f"Index doesn't exist")
+            
+    query_vector = model.embed_query(query_text)
 
-    results = [doc.page_content for doc in retriever]
-    return results
+    results = index.query(vector=query_vector, top_k=3, include_metadata=True)
+    found_text = []
+    for match in results['matches']:
+        text = match['metadata'].get('text', '')
+        score = match.get('score', 0)
+        if score >= 0:
+            found_text.append({"text": text, "score": score})
+
+
+    return found_text
+
+
+print(context("теорема Вейштрасса"))
