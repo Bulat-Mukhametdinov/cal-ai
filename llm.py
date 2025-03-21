457743d6cdb2d2c30bcbd8d10f9f11582cec2fc0
Elisei564 <batmansgrob@yandex.ru>
2025-03-21 14:02:26 +0300

    Changing llm and adding proof checking tool

--- llm.py
+++ llm.py
@@ -2,12 +2,17 @@ import streamlit as st
 from langchain_groq import ChatGroq
 
 MODEL_NAME = "llama3-70b-8192"
-
 llm = ChatGroq (
     model=MODEL_NAME, 
     api_key=st.secrets["GROQ_API_KEY"],
 )
 
+PROOF_MODEL = "deepseek-r1-distill-llama-70b"
+llm_proof = ChatGroq(
+    model=PROOF_MODEL,
+    api_key=st.secrets["GROQ_API_KEY"],
+)
+
 
 if __name__ == "__main__":
-    print(llm.invoke("Who are you?").content)
+    print(llm_proof.invoke("Who are you?").content)
--- tools.py
+++ tools.py
@@ -2,10 +2,14 @@ import streamlit as st
 from rag import context
 from langchain.tools import tool
 from pydantic import BaseModel, Field
+from llm import llm_proof
+
+### RAG search Tool begin ###
+
 
 class GetContext(BaseModel):
     query_text: str = Field(description="Text for searching in retrieval data base.")
-    
+
 
 @tool(args_schema=GetContext)
 def get_context(query_text: str) -> str:
@@ -13,8 +17,36 @@ def get_context(query_text: str) -> str:
     answer_list = context(query_text)
     return "\n".join(answer_list)
 
+### RAG search Tool end ###
 
+
+### Proof Check Tool begin ###
+
+
+prompt = """You are best at checking proof correctness
+Check this proof
+{proof}
+Return your short opinion on this proof"""
+
+
+class ProofCheck(BaseModel):
+    proof: str = Field(description="Proof that needs to be checked.")
+
+
+@tool(args_schema=ProofCheck)
+def proof_check(proof: str) -> str:
+    """Returns the correctness of proof"""
+    try:
+        correctness = llm_proof.invoke(prompt.format(proof = proof)).content.split('</think>')[-1]
+        return correctness
+    except Exception as e:
+        print("Error on checking proof")
+        print(e)
+        return "Error on checking proof"
+
+### Proof Check Tool end ###
 
 tools = [
     get_context,
+    proof_check,
 ]
