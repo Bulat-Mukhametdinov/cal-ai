358b9c9c0ff4714e452a3e1329a04fc90133cdc4
Мухаметдинов Булат Радикович <brmukhametdinov@edu.hse.ru>
2025-03-21 15:53:07 +0300

    added translator chain to agent

--- agents.py
+++ agents.py
@@ -1,12 +1,29 @@
 from tools import tools
 from llm import llm
 
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
 from langchain import hub
 from langchain.agents import AgentExecutor, create_react_agent
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 
 
 class AgentAnswerPipeline():
+    # Initialize translation prompt
+    translation_prompt = PromptTemplate(template=
+            "You are helpful assistant\n"
+            "User input: {input}\n"
+            "Response: {answer}\n"
+            "Answer with translated (if required or original) response. Before your answer say 'My answer:'\n",
+        input_variables=["input", "answer"]
+    )
+
+    def proccess_output(output):
+        return output.split("My answer: ")[-1]
+
+    # Create translation chain
+    translate = translation_prompt | llm | StrOutputParser() | proccess_output
+
     # Load the ReAct Agent Prompt
     react_docstore_prompt = hub.pull("hwchase17/react-chat")
 
@@ -20,7 +37,7 @@ class AgentAnswerPipeline():
     agent_executor = AgentExecutor.from_agent_and_tools(
         agent=agent, tools=tools, handle_parsing_errors=True,
     )
-    
+
     first_system_prompt = SystemMessage(content="You are helpful calculus assistant. Use latex format for all your formulas.")
 
     def __init__(self, debug=True):
@@ -34,11 +51,13 @@ class AgentAnswerPipeline():
         response = self.agent_executor.invoke(
             {"input": query, "chat_history": self.chat_history})
 
+        response = self.translate.invoke({"input": query, "answer": response["output"]})
+        
         # Update history
         self.chat_history.append(HumanMessage(content=query))
-        self.chat_history.append(AIMessage(content=response["output"]))
+        self.chat_history.append(AIMessage(content=response))
     
-        return response["output"]
+        return response
 
     def init_chat_history(self, history:list) -> None:
         self.chat_history = [self.first_system_prompt] + history
