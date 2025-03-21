d57c0ac7e57ad4ef8cde5d20b5584850e3ba5e50
Мухаметдинов Булат Радикович <brmukhametdinov@edu.hse.ru>
2025-03-21 16:51:12 +0300

    warning fixes in utils.py

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import random
 import os
 import json
 import time
-from langchain import PromptTemplate
+from langchain_core.prompts import PromptTemplate
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 
 # Save chat history to file
@@ -40,57 +40,57 @@ def generate_chat_name():
     return name
 
 
-#убираем все формулы(для программы Димона)
-def replace_formulas(model, text):
-    prompt_templ = """Ты — помощник по переводу математических формул в текст, который можно легко зачитать или озвучить. Твоя задача — преобразовать любое математическое выражение в простой текст, используя только буквы, цифры, пробелы и знаки препинания. Вот правила, которые нужно соблюдать:
+# #убираем все формулы(для программы Димона)
+# def replace_formulas(model, text):
+#     prompt_templ = """Ты — помощник по переводу математических формул в текст, который можно легко зачитать или озвучить. Твоя задача — преобразовать любое математическое выражение в простой текст, используя только буквы, цифры, пробелы и знаки препинания. Вот правила, которые нужно соблюдать:
 
-    1. Убери все специальные символы :
-        Замени символы $, ^, \sum, \infty, \frac и другие на слова или фразы.
-        Например, "\sum" замени на "сумма", "^n" замени на "в степени n", "\infty" замени на "до бесконечности".
-    2. Опиши формулу словами :
-        Объясни каждый элемент формулы так, будто ты рассказываешь это человеку.
-        Например, "f(x)" можно описать как "функция f от x".
-    3.Избегай сложных конструкций :
-        Используй только простые предложения, разделённые точками, запятыми или союзами.
-    Пример стиля :
-    Вход: f(x) = \sum_n=0^\infty \\fracf^(n)(a)n!(x - a)^n
-    Выход: Функция f от x равна сумме бесконечного ряда Каждый член ряда равен n-й производной функции f в точке a делённой на факториал числа n и умноженной на выражение x минус a в степени n Сумма берётся по всем значениям n начиная с нуля
-    Пример для практики :
-    Вход: e^x = \sum_n=0^\infty \\fracx^nn!
-    Выход: Экспонента от x равна сумме бесконечного ряда Каждый член ряда равен x в степени n делённому на факториал числа n Сумма берётся по всем значениям n начиная с нуля
-    Твоя задача :
-    Получить математическую формулу в виде текста.
-    Преобразовать её в простой текст, который можно легко зачитать или озвучить.
-    Убедиться, что в тексте нет специальных символов, только буквы, цифры, пробелы и знаки препинания. 
+#     1. Убери все специальные символы :
+#         Замени символы $, ^, \sum, \infty, \frac и другие на слова или фразы.
+#         Например, "\sum" замени на "сумма", "^n" замени на "в степени n", "\infty" замени на "до бесконечности".
+#     2. Опиши формулу словами :
+#         Объясни каждый элемент формулы так, будто ты рассказываешь это человеку.
+#         Например, "f(x)" можно описать как "функция f от x".
+#     3.Избегай сложных конструкций :
+#         Используй только простые предложения, разделённые точками, запятыми или союзами.
+#     Пример стиля :
+#     Вход: f(x) = \sum_n=0^\infty \\fracf^(n)(a)n!(x - a)^n
+#     Выход: Функция f от x равна сумме бесконечного ряда Каждый член ряда равен n-й производной функции f в точке a делённой на факториал числа n и умноженной на выражение x минус a в степени n Сумма берётся по всем значениям n начиная с нуля
+#     Пример для практики :
+#     Вход: e^x = \sum_n=0^\infty \\fracx^nn!
+#     Выход: Экспонента от x равна сумме бесконечного ряда Каждый член ряда равен x в степени n делённому на факториал числа n Сумма берётся по всем значениям n начиная с нуля
+#     Твоя задача :
+#     Получить математическую формулу в виде текста.
+#     Преобразовать её в простой текст, который можно легко зачитать или озвучить.
+#     Убедиться, что в тексте нет специальных символов, только буквы, цифры, пробелы и знаки препинания. 
                       
-    Выведи полностью следующе сообщение с заменнеными формулами и ничего больше, НИЧЕГО:
-    {message}"""
-    prompt = PromptTemplate(input_variables=['message'], template = prompt_templ)
-    messages = prompt.format(
-        message=text
-    )
-    response = model.invoke(messages).content
-    return response.split('</think>')[-1]
+#     Выведи полностью следующе сообщение с заменнеными формулами и ничего больше, НИЧЕГО:
+#     {message}"""
+#     prompt = PromptTemplate(input_variables=['message'], template = prompt_templ)
+#     messages = prompt.format(
+#         message=text
+#     )
+#     response = model.invoke(messages).content
+#     return response.split('</think>')[-1]
 
 
-#обработка текста, чтобы были разные цвета у размышлений и ответа
-def preprocess_think_tags(text): 
-    # Заменяем <think>...</think> на HTML с CSS-стилизацией
-    if '</think>' in text:
-        processed_text = text.replace("<think>", '<div style="color:red;">Размышления: </div><div style="font-size: 0.8em; opacity: 0.5;">')
-        processed_text = processed_text.replace("</think>", '</div><div style="color:red;">Ответ: </div>\n<div style="font-size: 1em;">')
-        processed_text += '\n</div>'
-    else:
-        processed_text = text.replace("<think>", '')
-        processed_text = '<span style="color: yellow; font-style: Roboto;">' + processed_text
-        processed_text += '</span>'
-    return processed_text
+# #обработка текста, чтобы были разные цвета у размышлений и ответа
+# def preprocess_think_tags(text): 
+#     # Заменяем <think>...</think> на HTML с CSS-стилизацией
+#     if '</think>' in text:
+#         processed_text = text.replace("<think>", '<div style="color:red;">Размышления: </div><div style="font-size: 0.8em; opacity: 0.5;">')
+#         processed_text = processed_text.replace("</think>", '</div><div style="color:red;">Ответ: </div>\n<div style="font-size: 1em;">')
+#         processed_text += '\n</div>'
+#     else:
+#         processed_text = text.replace("<think>", '')
+#         processed_text = '<span style="color: yellow; font-style: Roboto;">' + processed_text
+#         processed_text += '</span>'
+#     return processed_text
 
-#заменяем скобки для латеха
-def render_text_with_latex(text):
-    for symb in ['\[', '\]', '\(', '\)']:
-        text = text.replace(symb, '$')
-    return text
+# #заменяем скобки для латеха
+# def render_text_with_latex(text):
+#     for symb in ['\[', '\]', '\(', '\)']:
+#         text = text.replace(symb, '$')
+#     return text
 
 #делаем пословный вывод
 def write_with_delay(text):
